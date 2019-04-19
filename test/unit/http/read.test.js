03487aa6235c7f8e007d5bbbecc11c3ee7056114
Tucker Bickler <tuckersworld@gmail.com>
2019-04-19 11:13:02 -0500

    Add missing unit test

---
+++ test/unit/http/read.test.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Sinon = require('sinon')
+const Wreck = require('wreck')
+const { assert } = require('chai')
+
+const Read = require('../../../lib/http/read')
+
+describe('read', () => {
+  let suite
+
+  beforeEach(() => {
+    suite = {}
+    suite.sandbox = Sinon.createSandbox()
+    suite.readStub = suite.sandbox.stub(Wreck, 'read')
+  })
+
+  afterEach(async () => {
+    if (suite.server) {
+      await suite.server.stop({ timeout: 1 })
+    }
+    suite.sandbox.restore()
+    suite = null
+  })
+
+  it('should call Wreck.read', async function () {
+    await Read()
+    Sinon.assert.calledOnce(suite.readStub)
+  })
+
+  it('should call Wreck.read and pass in the provided `response` and `options` arguments', async function () {
+    await Read({ complete: true }, { json: true })
+    Sinon.assert.calledOnce(suite.readStub)
+    assert.equal(true, suite.readStub.getCall(0).args[0].complete)
+    assert.equal(true, suite.readStub.getCall(0).args[1].json)
+  })
+})
